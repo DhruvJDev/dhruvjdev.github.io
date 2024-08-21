7c1026e907d3ace22a3d95ecd5c15b93380a4347
DhruvJDev <djindal015@gmail.com>
2024-08-22 00:22:20 +0530

    modify resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -62,8 +62,9 @@ const experience = {
     items: [
         {
             company: "Excellence Technology Pvt. Ltd.",
-            position: "Training",
+            position: "Industrial Training",
             duration: "May 2024 - July 2024",
+            time: "3 mos",
         },
         // {
         //     company: "Excellence Technology Pvt. Ltd.",
@@ -279,7 +280,7 @@ const Resume = () =>
                                     <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
                                         {experience.items.map((item, index) => (
                                             <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
-                                                <span className="text-accent">{item.duration}</span>
+                                                <span className="text-accent">{item.duration} <span className="text-white/60">( {item.time} )</span></span>
                                                 <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
                                                 <div className="flex items-center gap-3">
                                                     <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
