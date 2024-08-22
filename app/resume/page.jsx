90fdd8dc042166bc7503c03265d4a75291dd5fac
DhruvJDev <djindal015@gmail.com>
2024-08-22 16:41:52 +0530

    update resume

--- app/resume/page.jsx
+++ app/resume/page.jsx
@@ -64,14 +64,8 @@ const experience = {
             company: "Excellence Technology Pvt. Ltd.",
             position: "Industrial Training",
             duration: "May 2024 - July 2024",
-            time: "3 mos",
+            time: "3mos",
         },
-        // {
-        //     company: "Excellence Technology Pvt. Ltd.",
-        //     position: "Training",
-        //     duration: "May 2024 - July 2024",
-        // },
-
     ],
 };
 
@@ -280,11 +274,11 @@ const Resume = () =>
                                     <ul className="grid grid-cols-1 lg:grid-cols-2 gap-[30px]">
                                         {experience.items.map((item, index) => (
                                             <li key={index} className="bg-[#232329] h-[184px] py-6 px-10 rounded-xl flex flex-col justify-center items-center lg:items-start gap-1">
-                                                <span className="text-accent">{item.duration} <span className="text-white/60">( {item.time} )</span></span>
+                                                <span className="text-accent">{item.duration} <br /><span className="text-white/60">({item.time})</span></span>
                                                 <h3 className="text-xl max-w-[260px] min-h-[60px] text-center lg:text-left">{item.position}</h3>
                                                 <div className="flex items-center gap-3">
                                                     <span className="w-[6px] h-[6px] rounded-full bg-accent"></span>
-                                                    <p className="text-white/60">{item.company}</p>
+                                                    <p className="text-white/60 text-[1rem]">{item.company}</p>
                                                 </div>
                                             </li>
                                         ))}
